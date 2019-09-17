428fa085338ff8c58092a5c7de0aef7e5b1e9d6d
S. Luo <p285253@fwn-bborg-129-125-5-140.fwn.rug.nl>
2019-09-17 20:54:41 +0200

    add readme.txt

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/p285253/ur5e_plugins/src/forward_kinematics
-# Build directory: /home/p285253/ur5e_plugins/src/forward_kinematics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "forward_kinematics"
-PROJECT_SPACE_DIR = "/home/p285253/ur5e_plugins/src/forward_kinematics/build/devel"
-PROJECT_VERSION = "0.0.0"
